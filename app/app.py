f60bb8ef246feef7a1f0712e5b13288b4828a2da
kollerbud <38870258+kollerbud@users.noreply.github.com>
2023-02-24 05:28:43 +0000

    datetime problem

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
+import datetime
 import pandas as pd
 import streamlit as st
 from load import load_data
 
 
-st.cache_data()
+@st.cache_data
 def raw_data() -> pd.DataFrame:
     '''
     this step is to load data from bigquery
@@ -11,25 +12,50 @@ def raw_data() -> pd.DataFrame:
     of THIS week
     caching to reduce Bigquery usage
     '''
+    print('running bq raw data')
     return pd.DataFrame(load_data())
 
 
 def ingest_df(station=None) -> pd.DataFrame:
     '''
+    select columns of interest and staging for
+    particular stations
     '''
 
     # load data from cache
     df = raw_data()
     # remove extra columns
-    df = df.loc[:, ['rn', 'destNM', 'nextStaNm',
-                    'arrT', 'resp_time', 'isApp',
+    df = df.loc[:, ['rn', 'destNm', 'nextStaNm',
+                    'arrT', 'isApp',
                     ]
                 ]
     # return only when the train is approaching station
     df = df.query('`isApp`=="1" & `nextStaNm`==@station')
-    return df
+    # re-define destination to NW & W, since not all route
+    # goes to the end
+    df['destNM'] = df['destNM'].map({"O'Hare": 'NW',
+                                     'Forest Park': 'W',
+                                     'UIC-Halsted': 'NW',
+                                     })
+
+    return df.reset_index(drop=True)
 
 
+def arrival_freq(func):
+
+    # ingested data
+    _df = func
+    
+    # split into morning & afternoon
+    df_morning = _df.loc[
+        (_df['arrT'])
+    ]
+
+
+    return None
+
+print(calc_timeseries())
+'''
 list_of_stations = ["O'Hare", 'Rosemont', 'Cumberland', "Harlem (O'Hare Branch)",
                     "Jefferson Park", "Montrose", 'Irving Park', 'Addison',
                     'Belmont', 'Logan Square', 'California',
@@ -51,6 +77,9 @@ with st.sidebar:
         options=list_of_stations,
         )
 
+st.dataframe(calc_timeseries())
+
 df_station = (ingest_df(station=station_picked).
               sort_values(by=['destNM', 'arrT'], ascending=[True, False]))
 st.dataframe(df_station)
+'''
