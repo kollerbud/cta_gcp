2a7cb1dacfc2490a315d5f9db3f01b4da51289c2
kollerbud <38870258+kollerbud@users.noreply.github.com>
2023-03-07 22:31:56 +0000

    add path

--- tests/test_src.py
+++ tests/test_src.py
@@ -1,19 +1,21 @@
-from __future__ import absolute_import
 import pytest
-from cta_gcp.src.cta_api import TrainStop
-from cta_gcp.src.data_to_bq import StoreToDB
+import sys
+sys.path.append('/workspaces/cta_gcp')
+from src.cta_api import TrainStop
+from src.data_to_bq import StoreToDB
+
 
 def test_api_output():
     # test fields return from API call
-    
+
     # blueline fields
     blue = TrainStop(method='blueline').method_response()
-    
+
     assert list(blue[0].keys()) == ['rn', 'destSt', 'destNm', 'trDr',
                             'nextStaId', 'nextStpId', 'nextStaNm',
                             'prdt', 'arrT', 'isApp', 'isDly', 'flags',
                             'lat', 'lon', 'heading', 'resp_time']
-    
+
 
 def test_bq_table():
     # make sure table already exist when try to create table
